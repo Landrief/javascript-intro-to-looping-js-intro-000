1b1335237d9a2937bed36f29fd7b830d60b80acd
Aranya Gaddam <agaddam1@gmail.com>
2018-05-12 19:00:44 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,3 +1,5 @@
-for (var i = 1; i < 25; i++) {
-  console.log( "Hello World the " + i + " time" );
+for (let i = 0; i < 25; i++) {
+  if(console.log("I am 1 strange loop."));
+
+  else(console.log("I am ${i} strange loops."));
 }
